e320143e140efa2404a2a2cc0195859efd2afe38
Javier Salado Ramos <jsalado@bebeyond.es>
2023-03-08 09:04:25 +0100

    Mejorado script init.sh para que realice todos los comandos de una sola pasada

--- init.sh
+++ init.sh
@@ -12,11 +12,14 @@ rm -rf .git .gitignore symfony/.gitkeep mysql/.gitignore
 result=${PWD##*/}
 result=${result:-/}
 
-## Declaramos las funciones necesarias para la ejecución ##
+#################################
+########### FUNCIONES ###########
+#################################
 function change_directory() {
     echo "Indica el nombre del proyecto:"
     read proyecto
-    if [[ ! -z "$proyecto" ]] && mv "../${PWD##*/}" "../$proyecto-docker"
+    if [[ ! -z "$proyecto" ]] && \
+    mv "../${PWD##*/}" "../$proyecto-docker"
     then
         echo "Nombre del proyecto modificado con éxito."
     fi
@@ -24,7 +27,8 @@ function change_directory() {
 function clone_project() {
     echo "Introduce la url del proyecto que deseas clonar:"
     read git
-    if [[ ! -z "$git" ]] && git clone "$git" ./symfony
+    if [[ ! -z "$git" ]]
+    \ && git clone "$git" ./symfony
     then
         echo "Proyecto clonado con éxito."
     fi
@@ -32,12 +36,51 @@ function clone_project() {
 function change_database() {
     echo "Indica el nombre de la base de datos que deseas inicializar:"
     read db
-    if [[ ! -z "$db" ]] && sed -i "s/DATABASE_NAME/$db/g" docker-compose.yml && mv mysql/*.sql "mysql/$db.sql"
+    if [[ ! -z "$db" ]] && \
+    sed -i "s/DATABASE_NAME/$db/g" docker-compose.yml && \
+    mv mysql/*.sql "mysql/$db.sql"
     then
         echo "Base de datos renombrada con éxito."
     fi
 }
 
+function import_database() {
+    echo "Importando base de datos..."
+    echo "Creando la base de datos..."
+    if docker exec -i db mysql -uroot -proot -e "CREATE DATABASE $db;"
+    then
+        echo "Base de datos creada con éxito."
+        echo "Importando los datos..."
+        if docker exec -i db mysql -uroot -proot $db < ./mysql/$db.sql
+        then
+            echo "Base de datos importada con éxito."
+        fi
+    fi
+}
+
+function docker_compose_up() {
+    echo "Levantando contenedores..."
+    if docker compose up -d --build
+    then
+        echo "Finalizando..."
+        echo "..."
+        sleep 2
+        echo "..."
+        sleep 2
+        echo "Contenedores levantados con éxito."
+        printf "\n"
+        
+        read -p "¿Deseas importar la base de datos? (Y/n) " resp
+        if [[ "$resp" =~ y|Y|"" ]]
+        then
+            import_database
+        fi
+    fi
+}
+
+#################################
+########### RECORRIDO ###########
+#################################
 ## Modificamos el nombre del proyecto de docker ##
 if [[ "${PWD##*/}" =~ symfony-docker ]]
 then
@@ -56,7 +99,7 @@ if [ -z "$(ls -A ./symfony)" ]
 then
     clone_project
 else
-    read -p "El directorio symfony ya tiene contenido. ¿Deseas eliminarlo (y/N)?" resp
+    read -p "El directorio symfony ya tiene contenido. ¿Deseas eliminarlo? (y/N) " resp
     if [[ "$resp" =~ y|Y ]]
     then
         rm -rf symfony
@@ -67,4 +110,19 @@ fi
 printf "\n"
 
 ## Modificamos el nombre genérico de la base de datos ##
-change_database
+if grep -q DATABASE_NAME docker-compose.yml
+then
+    change_database
+fi
+printf "\n"
+
+## Levantamos los contenedores e importamos la base de datos ##
+read -p "El proyecto se encuentra preparado. ¿Deseas levantar los contenedores? (Y/n) " resp
+if [[ "$resp" =~ y|Y|"" ]]
+then
+    docker_compose_up
+fi
+printf "\n"
+
+echo "RECUERDA actualizar las dependencias del proyecto y modificar el fichero '.env'"
+echo "Ya puedes acceder a través del siguiente enlace: http://localhost:8080"
